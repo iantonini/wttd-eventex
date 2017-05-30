6ec9b617abd346e98911cfe0ccc94c10a2d1da21
linux <iantonini.m@gmail.com>
2017-05-30 18:47:55 -0300

    Add testes - Docs

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -5,8 +5,10 @@ class Hometest(TestCase):
         self.response = self.client.get('/')
 
     def test_get(self):
+        """GET / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_home(self):
+        """Must use index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
 
