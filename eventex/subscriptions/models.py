e1bdc09a32ecc0d63a21005aabe8ecd94be07b1d
linux <iantonini.m@gmail.com>
2017-06-27 10:02:21 -0300

    teste heroku

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('nome', max_length=100)
+    cpf = models.CharField('cpf', max_length=11)
+    email = models.EmailField('e-mail')
+    phone = models.CharField('telefone', max_length=20)
+    created_at = models.DateTimeField('inscrito hoje?', auto_now_add=True)
