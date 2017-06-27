6cd3e77439226672bea1b12593cb1e347dbbf3c8
linux <iantonini.m@gmail.com>
2017-06-27 11:34:47 -0300

    Admin magic

---
+++ eventex/subscriptions/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.utils.timezone import now
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptioModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'cpf', 'created_at', 'subscribed_today')
+    date_hierarchy = 'created_at'
+    search_fields = ('name', 'email', 'cpf', 'created_at')
+    list_filter = ('created_at',)
+
+    def subscribed_today(self, obj):
+        return obj.created_at.date() == now().date()
+
+    subscribed_today.short_description = 'Inscrito hoje?'
+    subscribed_today.boolean = True
+
+admin.site.register(Subscription, SubscriptioModelAdmin)
--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -6,4 +6,13 @@ class Subscription(models.Model):
     cpf = models.CharField('cpf', max_length=11)
     email = models.EmailField('e-mail')
     phone = models.CharField('telefone', max_length=20)
-    created_at = models.DateTimeField('inscrito hoje?', auto_now_add=True)
+    created_at = models.DateTimeField('Data de Criação', auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'inscrições'
+        verbose_name = 'inscrição'
+        ordering = ('-created_at',)
+
+
+    def __str__(self):
+        return self.name
